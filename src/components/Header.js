e36f1bdabef6fb5a6607c136d0af32373587d36c
JustJeo <j.cabrigas.writer@gmail.com>
2021-04-07 15:54:46 -0700

    removes Footer and right aligns social links

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,13 +5,17 @@ import "./Header.scss"
 const Header = () => {
   return (
     <header>
-      <div className = "navLinks">
-        <ul>
+      <div>
+        <ul className = "navLinks">
           <li><Link to = { "/" }>Home</Link></li>
           <li><Link to = { "/monochrome" }>Monochrome</Link></li>
           <li><Link to = { "/cheetah" }>Cheetah</Link></li>
           <li><Link to = { "/holidays" }>Holidays</Link></li>
           <li><Link to = { "/shoes" }>Shoes</Link></li>
+        </ul>
+        <ul className = "socials">
+          <li><Link to = { "https://www.liketoknow.it/stephslookbook" }>Like To Know It</Link></li>
+          <li><Link to = { "https://www.instagram.com/stephslookbook/" }>Instagram</Link></li>
         </ul>
       </div>
     </header>
