ff633da9bdbda9dcf47703bc0f604d7df5b5d6ca
JustJeo <j.cabrigas.writer@gmail.com>
2021-04-07 15:16:54 -0700

    styles footer and updates About Me section with accurate info

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,9 +6,12 @@ const Home = () => {
       <h1>Welcome to Steph's Lookbook!</h1>
       <div className = "aboutMe">
         <h2>About Me</h2>
-        <p>Fill in Steph's basic about me.</p>
+        <p>Just a girl with a big love for fashion and an even bigger love for God 💕 <br />Height: 5’3 <br />
+        Size: M <br />
+        Instagram: @stephslookbook. <br />
+        Like To Know It: /stephslookbook</p>
       </div>
-      <div className = "support">
+      <div className = "supportMe">
         <h2>Support Me</h2>
         <div className = "likeToKnowIt">
           <h3>Like To Know It</h3>
